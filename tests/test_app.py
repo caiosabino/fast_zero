a3424f01e5cd01079dcdfd44f7792813f8d84503
Caio Sabino <caio.sabino@mercadolivre.com>
2025-08-07 08:15:08 -0300

    feat: aula 2 com exercicio

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,29 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'hello world'}  # Asset
+
+
+def test_html_deve_retornar_ok():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/html')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert (
+        response.headers['content-type'] == 'text/html; charset=utf-8'
+    )  # Asset
+
+
+def test_user_deve_retornar_ok():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/user')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {
+        'id': 1,
+        'name': 'John Doe',
+        'email': 'john.doe@example.com',
+        'age': 30,
+        'is_admin': False,
+    }  # Asset
